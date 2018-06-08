53476dd515bcf27d47643b396f2f51222aac3632
Marcel Hollerbach <mail@marcel-hollerbach.de>
2018-06-08 16:07:57 +0200

    apply clang-format on each

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -1,54 +1,54 @@
 #include "e.h"
 #include "quickscreen.h"
 
-
-static E_Action *act;
+static E_Action* act;
 
 int _quickscreen_log_domain = -1;
 
-E_API E_Module_Api e_modapi =
-{
-   E_MODULE_API_VERSION,
-     "Quickscreen"
-};
-
+E_API E_Module_Api e_modapi = { E_MODULE_API_VERSION, "Quickscreen" };
 
 static void
-qs_key(E_Object *obj EINA_UNUSED, const char *params EINA_UNUSED)
+qs_key(E_Object* obj EINA_UNUSED, const char* params EINA_UNUSED)
 {
   printf("QUICKSCREEN\n");
 }
 
-E_API void *
-e_modapi_init(E_Module *m)
+E_API void*
+e_modapi_init(E_Module* m)
 {
   _quickscreen_log_domain = eina_log_domain_register("quickscreen", "red");
 
   act = e_action_add("qs_key");
-  e_action_predef_name_set("Quickscreen", "Toogle Screenlayout Popup", "qs_key", NULL, NULL, 0);
+  e_action_predef_name_set(
+    "Quickscreen", "Toogle Screenlayout Popup", "qs_key", NULL, NULL, 0);
   act->func.go = qs_key;
 
-  e_configure_registry_item_add("extensions/screen2", 0, "Screen Settings2", NULL, NULL, quickscreen_settings);
+  e_configure_registry_item_add("extensions/screen2",
+                                0,
+                                "Screen Settings2",
+                                NULL,
+                                NULL,
+                                quickscreen_settings);
 
   return m;
 }
 
 E_API int
-e_modapi_shutdown(E_Module *m EINA_UNUSED)
+e_modapi_shutdown(E_Module* m EINA_UNUSED)
 {
-   /* your code-shutdown */
+  /* your code-shutdown */
 
-   E_FREE_FUNC(act, e_action_del);
-   e_action_predef_name_del("Quickscreen", "qs_key");
+  E_FREE_FUNC(act, e_action_del);
+  e_action_predef_name_del("Quickscreen", "qs_key");
 
-   e_configure_registry_item_del("extensions/screen2");
+  e_configure_registry_item_del("extensions/screen2");
 
-   return 1;
+  return 1;
 }
 
 E_API int
-e_modapi_save(E_Module *m EINA_UNUSED)
+e_modapi_save(E_Module* m EINA_UNUSED)
 {
-   /* your code-save */
-   return 1;
+  /* your code-save */
+  return 1;
 }
