014bdab0809b46e1f54137302b4d706ab414951a
simon <simon@localhost>
2018-10-23 19:49:21 +0200

    show quickscreen on all zones

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -5,6 +5,8 @@ static E_Action* act;
 
 int _quickscreen_log_domain = -1;
 
+/*List of Mirrored Popups*/
+Eina_List *popups, *mirrors;
 E_API E_Module_Api e_modapi = { E_MODULE_API_VERSION, "Quickscreen" };
 
 static Evas_Object* popup;
@@ -14,10 +16,17 @@ _block_clicked_cb(void* data EINA_UNUSED,
                   Evas_Object* obj,
                   void* event_info EINA_UNUSED)
 {
+  Evas_Object *pup;
+
   evas_object_del(popup);
   popup = NULL;
 
   e_comp_ungrab_input(1, 1);
+  EINA_LIST_FREE(popups, pup)
+  {
+     evas_object_del(pup);
+     pup = NULL;
+  }
 }
 
 void
@@ -28,60 +37,152 @@ key_down(void* data EINA_UNUSED,
 {
   Evas_Event_Key_Down* ev = event_info;
   const char* k = ev->keyname;
+  Evas_Object *pup;
 	
   if (!strcmp(k, "Escape")) {
     if (popup) {
       evas_object_del(popup);
       popup = NULL;
       e_comp_ungrab_input(1, 1);
+      EINA_LIST_FREE(popups, pup)
+      {
+         evas_object_del(pup);
+         pup = NULL;
+      }
     }
   }
 }
 
-
 static void
-display_popup(Evas_Object* content)
+popup_resized(void *data, Evas *e EINA_UNUSED, Evas_Object *obj, void *event_data EINA_UNUSED)
 {
-  if (popup) {
-    evas_object_del(popup);
-    popup = NULL;
-    e_comp_ungrab_input(1, 1);
-  }
+   E_Zone *zone; Eina_List *l = NULL;
+   Evas_Object *popup = data, *pup, *mirror;
+   /*Remove previous mirrors*/
+   EINA_LIST_FREE(mirrors, mirror)
+   {
+      evas_object_del(mirror);
+      mirror = NULL;
+   }
+   /*Remove previous mirrored popups*/
+   EINA_LIST_FREE(popups, pup)
+   {
+      evas_object_del(pup);
+      pup = NULL;
+   }
+   printf("POPUP RESIZE\n");
+		
+   int i = 0;
+   int h = 0;
+   int w = 0;
+	
+   EINA_LIST_FOREACH(e_comp->zones, l, zone) 
+   {
+      if (i == 0) 
+	{
+           /*center popup on first zone*/
+           e_comp_object_util_center_on_zone(popup, zone); 
+           evas_object_show(popup);
+        }
+      else 
+        {
+           /*create mirror for each additional zone*/
+           mirror = evas_object_image_filled_add(e_comp->evas);
+           evas_object_image_source_set(mirror, obj);
+           evas_object_image_source_events_set(mirror, EINA_TRUE);
 
-  elm_config_focus_highlight_enabled_set(EINA_TRUE);
-  
-  popup = elm_popup_add(e_comp->elm);
-  e_comp_grab_input(1, 1);
+           pup = elm_popup_add(e_comp->elm);
+           evas_object_geometry_get(obj, 0, 0, &w, &h);
+           evas_object_size_hint_min_set(mirror, w, h);
+           elm_object_style_set(pup, "transparent");
+           evas_object_layer_set(pup, E_LAYER_CLIENT_ABOVE);
+           evas_object_layer_set(pup, E_LAYER_POPUP);
 
-  elm_object_style_set(popup, "transparent");
-//       evas_object_layer_set(popup, E_LAYER_CLIENT_ABOVE);
-  evas_object_layer_set(popup, E_LAYER_POPUP);
+           evas_object_smart_callback_add(
+           pup, "block,clicked", _block_clicked_cb, popup);
+				
+           elm_object_content_set(pup, mirror); 
 
-  evas_object_smart_callback_add(
-    popup, "block,clicked", _block_clicked_cb, popup);
+           e_comp_object_util_center_on_zone(pup, zone);
+           evas_object_show(mirror);
+           evas_object_show(pup);
 
-  evas_object_event_callback_add(
-    popup, EVAS_CALLBACK_KEY_DOWN, key_down, popup);
-
-  elm_object_content_set(popup, content);
-  evas_object_show(popup);
-
+           /*append mirror to list of mirrors*/
+           mirrors = eina_list_append(mirrors, mirror);
+           /*append popoup to list of mirrored popups*/
+           popups = eina_list_append(popups, pup);
 
 
-  // das muss sein :)
-  evas_object_data_set(popup, "__data", NULL);
+           if (!mirror) printf("Mirror is null\n");
+// 					printf("QUICKSCREEN MIRROR\n");
+        } 
+      i++;
+   }
 }
 
 static void
 qs_key(E_Object* obj EINA_UNUSED, const char* params EINA_UNUSED)
-{
+{ 
+   Evas_Object* content;
+   E_Zone *zone; Eina_List *l = NULL; 
+  
+   content = wizard_config_create(e_comp->elm);
+//   display_popup(content);
+   popup = elm_popup_add(e_comp->elm);
+//    e_comp_grab_input(1, 1);
 
-  Evas_Object* content;
+   elm_object_style_set(popup, "transparent");
+   evas_object_layer_set(popup, E_LAYER_CLIENT_ABOVE);
+   evas_object_layer_set(popup, E_LAYER_POPUP);
 
-  content = wizard_config_create(e_comp->elm);
-  display_popup(content);
+   evas_object_smart_callback_add(
+   popup, "block,clicked", _block_clicked_cb, popup);
 
-  printf("QUICKSCREEN\n");
+   evas_object_event_callback_add(
+   popup, EVAS_CALLBACK_KEY_DOWN, key_down, popup);
+
+   /*add resize callback for popup*/
+   evas_object_event_callback_add(content, EVAS_CALLBACK_RESIZE, popup_resized, popup);
+					
+   elm_object_content_set(popup, content); 
+					
+   int i = 0;
+	
+   EINA_LIST_FOREACH(e_comp->zones, l, zone) 
+     {
+        if (i == 0) 
+          {
+             e_comp_object_util_center_on_zone(popup, zone); 
+             evas_object_show(popup);
+          }
+        else 
+          {
+             Evas_Object *mirror = evas_object_image_filled_add(e_comp->evas);
+             evas_object_image_source_set(mirror, content);
+             evas_object_image_source_events_set(mirror, EINA_TRUE);
+
+             Evas_Object *pup = elm_popup_add(e_comp->elm);
+	     elm_object_style_set(pup, "transparent");
+	     evas_object_layer_set(pup, E_LAYER_CLIENT_ABOVE);
+	     evas_object_layer_set(pup, E_LAYER_POPUP);
+
+	     evas_object_smart_callback_add(
+	        pup, "block,clicked", _block_clicked_cb, popup);
+					
+             elm_object_content_set(pup, mirror); 
+
+             e_comp_object_util_center_on_zone(pup, zone);
+             evas_object_show(mirror);
+             evas_object_show(pup);
+
+             /*append mirror to list of mirrors*/
+             mirrors = eina_list_append(mirrors, mirror);
+             /*append popup to list of mirrored popups*/
+             popups = eina_list_append(popups, pup);
+             printf("QUICKSCREEN MIRROR\n");
+          } 
+        i++;
+     }
 }
 
 E_API void*
